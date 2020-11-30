6752e9c1f2b0c05f3191876fa4e60ee6ac781897
Joshua <51002097+wishh@users.noreply.github.com>
2020-11-30 16:08:21 +0000

    Update spark-dns.py

--- spark-dns.py
+++ spark-dns.py
@@ -3,7 +3,7 @@ import checkdmarc
 
 app = FastAPI()
 
-@app.get("/api/checkdmarc/{domain}")
+@app.get("/api/dnssearch/{domain}")
 async def check_domain(domain: str, response: Response):
     response.headers["Access-Control-Allow-Origin"] = "*"
     return checkdmarc.check_domains([domain], include_dmarc_tag_descriptions=True)
