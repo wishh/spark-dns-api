a3ecbdce8b41c0f8750afd1c351ea7d226d2cc68
Josh <51002097+wishh@users.noreply.github.com>
2020-12-09 17:03:47 +0000

    Update spark-dns.py

--- spark-dns.py
+++ spark-dns.py
@@ -1,11 +1,16 @@
+## Import Modules - Both are required inthe requirements.txt
 from fastapi import FastAPI, Response
 import checkdmarc
 
+## Initialise the application
 app = FastAPI()
 
+## Endpoint in which the API is hosted
 @app.get("/api/dnslookup/{domain}")
 async def check_domain(domain: str, response: Response):
-    response.headers["Access-Control-Allow-Origin"] = "*"
-    search = checkdmarc.check_domains([domain], include_dmarc_tag_descriptions=True)
+    response.headers["Access-Control-Allow-Origin"] = "*" ## Headers to check when a request is made
+    search = checkdmarc.check_domains([domain], include_dmarc_tag_descriptions=True) ## What happens when the request is made
 
-    return search
+    return search ## Return the results of the search on the given domain.
+
+## Feel free to test at https://development.joshh.me/api/dnslookup/ignition-technology.com
